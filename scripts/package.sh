4a484eb2a15e17b734af52a03f9c9313651cf735
Shibbydex Extended <shibbydexx@gmail.com>
2023-01-30 20:27:37 +0800

    package script now creates chrome and firefox versions

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,4 +2,7 @@
 mkdir -p ./dist
 zip -r shibbydexx-source.zip *
 npx webpack
-cd dist/ && zip -r -FS ../shibbydexx.zip * && cd ..
+cd dist/ && zip -r -FS ../shibbydexx-firefox.zip * && cd ..
+rm dist/manifest.json
+cp manifest-v3.json dist/manifest.json
+cd dist/ && zip -r -FS ../shibbydexx-chrome.zip * && cd ..
