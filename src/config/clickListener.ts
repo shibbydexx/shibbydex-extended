fab3ab5fd12681bba6f857d4e00138e66ac43e0f
Shibbydex Extended <shibbydexx@gmail.com>
2023-03-29 21:49:05 +0800

    fix chrome options page using event listeners for manifest v3

--- src/config/clickListener.ts
+++ src/config/clickListener.ts
@@ -1,9 +1,26 @@
-import browser from "webextension-polyfill"
-
+import browser from "webextension-polyfill";
 // listen for the browser action being clicked and then open the config page
-browser.browserAction.onClicked.addListener(() => {
-  const optionsUrl = browser.runtime.getURL('config/config.html')
-  browser.tabs.create({
-    url: optionsUrl
+//
+
+// Firefox does not support service workers yet for manifest v3 so we need
+// to keep this manifest v2 implementation for now
+if(browser.browserAction) {
+  // manifest v2
+  browser.browserAction.onClicked.addListener(() => {
+    const optionsUrl = browser.runtime.getURL('config/config.html')
+    browser.tabs.create({
+      url: optionsUrl
+    })
   })
-})
+} else {
+  // manifest v3
+  browser.runtime.onInstalled.addListener(() => {
+    browser.action.onClicked.addListener(async () => {
+      const optionsUrl = browser.runtime.getURL("config/config.html");
+      await browser.tabs.create({
+        url: optionsUrl,
+      });
+    });
+  });
+}
+
